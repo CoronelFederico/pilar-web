5af6a316fb3cdee51496ab9531172f19bc9296fb
Coronel Federico <coronelfedericod@gmail.com>
2022-11-05 20:02:19 -0300

    update: error en linea 129:20 (fetclist.js)

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,69 @@
+import axios from "axios";
+
+const URL_BASE = "https://pokeapi.co/api/v2";
+
+export const IMG_URL = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/";
+
+
 const headers = () => {
-    const headers = {
+  const headers = {
     headers: {
-    'content-Types': 'aplication/json'
-    }
-    }
-    return headers
-    }
+      "content-Types": "aplication/json",
+    },
+  };
+  return headers;
+};
+
+const errorMessage = {
+  message: "Error en el servidor",
+  name: "serverError",
+  statusCode: 500,
+};
+
+const POST = async (url, payload) => {
+  let res = null;
+  try {
+    res = await axios.post(url, payload, headers());
+    return (res && res.data) || null;
+  } catch (error) {
+    throw (error && error.response.data.error) || errorMessage;
+  }
+};
+
+const GET = async (url) => {
+  let res = null;
+  try {
+    res = await axios.post(url, headers());
+    return (res && res.data) || null;
+  } catch (error) {
+    throw (error && error.response.data.error) || errorMessage;
+  }
+};
+
+const PATCH = async (url, payload) => {
+  let res = null;
+  try {
+    res = await axios.patch(url, payload, headers());
+    return (res && res.data) || null;
+  } catch (error) {
+    throw (error && error.response.data.error) || errorMessage;
+  }
+};
+
+const DELETE = async (url) => {
+  let res = null;
+  try {
+    res = await axios.delete(url, headers());
+    return (res && res.data) || null;
+  } catch (error) {
+    throw (error && error.response.data.error) || errorMessage;
+  }
+};
+
+export default {
+  POST,
+  GET,
+  PATCH,
+  DELETE,
+  pokemons:`${URL_BASE}/pokemon`
+};
